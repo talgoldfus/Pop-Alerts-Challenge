55c94b33c6802e71dbf53c22d44c472571fbbff4
Tal Goldfus <talgoldfus@gmail.com>
2016-09-12 10:09:31 -0400

    Removed focus outline from inputs and refractured css files

--- Pop-Alerts/src/components/Menu.js
+++ Pop-Alerts/src/components/Menu.js
@@ -1,7 +1,6 @@
 import React,{Component}from 'react';
 import {Link ,withRouter } from 'react-router'
 
-
 class Menu extends Component{
 
 render(){
@@ -14,12 +13,12 @@ render(){
     ]
 
     const links= menueLinks.map((link)=>{
-      if (this.props.location.pathname != link.path){
+      if (this.props.location.pathname !== link.path){
         return  (
           <div key={link.text} className="row" >
             <Link
               to={link.path}
-              className="menuBtn col-md-10"
+              className="menuBtn col-xs-10 col-sm-10 col-md-10"
               onClick={this.props.display}>
               {link.text}
             </Link>
@@ -30,7 +29,7 @@ render(){
 
     return (
       <div className="row" >
-        <div className="col-md-offset-9 col-md-2 menu">
+        <div className="col-xs-offset-4 col-xs-7 col-sm-offset-6 col-sm-5 col-md-offset-9 col-md-2 menu">
           <div className="menuWrapper">
             {links}
           </div>
